004360756458e3b5b5817ee4ed9616e7c4327415
Giordano Zanoli <giordano.zanoli84@gmail.com>
2021-04-01 18:16:57 +0200

    add:: second walkthrough implemented

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -31,6 +31,7 @@ that we have created in the `__init__` function.
 
 '''
 
+
 class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
@@ -54,25 +55,61 @@ class DBWNode(object):
                                          BrakeCmd, queue_size=1)
 
         # TODO: Create `Controller` object
-        # self.controller = Controller(<Arguments you wish to provide>)
+        self.controller = Controller(
+            vehicle_mass=vehicle_mass,
+            fuel_capacity=fuel_capacity,
+            brake_deadband=brake_deadband,
+            decel_limit=decel_limit,
+            accel_limit=accel_limit,
+            wheel_radius=wheel_radius,
+            wheel_base=wheel_base,
+            steer_ratio=steer_ratio,
+            max_lat_accel=max_lat_accel,
+            max_steer_angle=max_steer_angle,
+        )
 
         # TODO: Subscribe to all the topics you need to
+        rospy.Subscriber(
+            '/current_velocity', TwistStamped, self.cb_current_velocity)
+        rospy.Subscriber(
+            '/twist_cmd', TwistStamped, self.cb_twist)
+        rospy.Subscriber(
+            '/vehicle/dbw_enabled', Bool, self.cb_dbw_enabled)
+
+        self.current_vel = None
+        self.current_ang_vel = None
+        self.dbw_enabled = None
+        self.linear_vel = None
+        self.angular_vel = None
+        self.throttle = 0.0
+        self.steering = 0.0
+        self.brake = 0.0
 
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(50)  # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+            if None in (self.linear_vel, self.angular_vel, self.current_vel):
+                continue
+            throttle, brake, steer = self.controller.control(
+                self.linear_vel, self.angular_vel,
+                self.current_vel, self.dbw_enabled, )
+            if self.dbw_enabled:
+                self.publish(throttle, brake, steer)
             rate.sleep()
+
+    def cb_current_velocity(self, msg):
+        self.current_vel = msg.twist.linear.x
+
+    def cb_twist(self, msg):
+        self.linear_vel = msg.twist.linear.x
+        self.angular_vel = msg.twist.linear.z
+
+    def cb_dbw_enabled(self, msg):
+        self.dbw_enabled = msg
 
     def publish(self, throttle, brake, steer):
         tcmd = ThrottleCmd()
--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,78 @@
-
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+import rospy
+
+from yaw_controller import YawController
+from pid import PID
+from lowpass import LowPassFilter
+
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, vehicle_mass=None, fuel_capacity=None,
+                 brake_deadband=None, decel_limit=None, accel_limit=None,
+                 wheel_radius=None, wheel_base=None, steer_ratio=None,
+                 max_lat_accel=None, max_steer_angle=None):
 
-    def control(self, *args, **kwargs):
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+        self.wheel_base = wheel_base
+        self.steer_ratio = steer_ratio
+        self.max_lat_accel = max_lat_accel
+        self.max_steer_angle = max_steer_angle
+
+        self.controller_steering = YawController(
+            wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+        self.controller_throttle = PID(0.3, 0.1, 0.0, 0.0, 0.2)
+
+        self.lowpass = LowPassFilter(0.5, 1. / 50.)
+
+        self.last_time = rospy.get_time()
+        self.last_vel = 0.0
+
+    def control(self, linear_vel, angular_vel, current_vel, dbw_enabled):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
-        return 1., 0., 0.
+
+        if not dbw_enabled:
+            self.controller_throttle.reset()
+            return 0.0, 0.0, 0.0
+
+        # low pass the current velocity
+        current_vel = self.lowpass.filt(current_vel)
+
+        # update time
+        current_time = rospy.get_time()
+        step_time = current_time - self.last_time
+        self.last_time = current_time
+
+        # steering
+        steering = self.controller_steering.get_steering(
+            linear_vel, angular_vel, current_vel)
+
+        # velocity
+        vel_error = linear_vel - current_vel
+        self.last_vel = current_vel
+
+        # logic on throttle and brake
+        throttle = self.controller_throttle.step(vel_error, step_time)
+        brake = 0.0
+
+        if linear_vel == 0 and current_vel < 0.1:
+            # we want to stop
+            throttle = 0.0
+            brake = 700  # N*m
+
+        elif throttle < 0.1 and vel_error < 0.0:
+            # we want to decelerate
+            throttle = 0.0
+            decelerate = max(vel_error, self.decel_limit)
+            brake = abs(decelerate) * self.vehicle_mass * self.wheel_radius
+
+        rospy.logwarn('Throttle {} - Brake {} - Steer {}'.format(throttle, brake, steering))
+
+        return throttle, brake, steering
